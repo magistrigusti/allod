6d0fd547d07de281de7122c1e2c65a325d054e1b
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-08-01 20:29:12 +0300

    0.3.6

--- src/feature/FindPathMenu/FindPathMenu.tsx
+++ src/feature/FindPathMenu/FindPathMenu.tsx
@@ -1,4 +1,54 @@
+import "./FindPathMenu";
+import { Card, Input, Flex, Button, Alert, Form } from 'antd';
+import { Graph } from "@/shared/Graph";
+import { getIdsOfPathNodes } from "./util/getIdsOfPathNodes";
 
-export const FindPathMenu = () => {
-  
+type PropsType = {
+  housesPathGraph: Graph;
+  db: IndexDB;
+}
+
+export const FindPathMenu = ({ housesPathGraph, db }: PropsType) => {
+  const handleSearchPath = async (values: { from: string; to: string }) => {
+    const allHousesInfo = await db.getAllHousesInfo();
+
+    const { nodeFromId, nodeToId } = getIdsOfPathNodes(values.from, values.to, allHousesInfo);
+    
+    const nodeFrom = housesPathGraph.map.get(nodeFromId);
+    const nodeTo = housesPathGraph.map.get(nodeToId);
+
+    const hasNodes = nodeTo && nodeFrom;
+
+    if (!hasNodes) {
+      return;
+    }
+
+    const possiblePathMatrix = housesPathGraph.getAllPaths(nodeFrom, nodeTo);
+  };
+
+  return (
+    <Card rootClassName="findpath-container" title="fuind line">
+      <Form>
+        <Flex gap="middle" vertical>
+          <Form.Item name="from" 
+            style={{ marginBottom: 0 }}
+            rules={[{ required: true, message: "Fill"}]}
+          >
+            <Input placeholder="from" />
+          </Form.Item>
+
+          <Form.Item name="to" 
+            style={{marginBottom: 0}}
+            rules={[{ required: true, message: "Fill"}]}
+          >
+            <Input placeholder="from" />
+          </Form.Item>
+
+          <button htmlType="submit" type="primary">
+            fuind
+          </button>
+        </Flex>
+      </Form>
+    </Card>
+  )
 }
---
+++ src/feature/FindPathMenu/util/getIdsOfPathNodes.ts
@@ -0,0 +1,27 @@
+import { House } from "@/shared/House";
+
+export const getIdsOfPathNodes = (
+  houseNameFrom: string,
+  houseNameTo: string,
+  housesInfo: House[]
+) => {
+  return housesInfo.reduce(
+    (nodeFromToIds, info) => {
+      const { name, id } = info;
+
+      const isHouseFrom = name === houseNameFrom;
+      const isHouseTo = name === houseNameTo;
+
+       if (isHouseFrom) {
+        nodeFromToIds.nodeFromId = id;
+      }
+
+      if (isHouseTo) {
+        nodeFromToIds.nodeToId = id;
+      }
+
+      return nodeFromToIds;
+    },
+    { nodeFromId: '', nodeToId: '' }
+  )
+}
