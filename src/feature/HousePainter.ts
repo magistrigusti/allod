69acb831c69c6981ae322b28902ff58770cbddca
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-08-03 10:13:10 +0300

    0.4.3

--- src/feature/HousePainter.ts
+++ src/feature/HousePainter.ts
@@ -6,26 +6,100 @@ import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { assetsConfig } from '@/constants/assetsConfig';
 
 export class HousePainter {
+  private indexDB = new IndexDB();
   private draftHouse: House | null = null;
-  private houseFrom: House | null = null;
-  private indexDd = new IndexDB();
+  private helperArmPlane: Mesh | null = null;
+
+  housesMap = new Map<string, House>();
 
   constructor(private sceneConnector: SceneConnector, private assetMap: Map<string, GLTF>) {
     this.assetMap = assetMap;
 
-    window.addEventListener('dblclick', this.handleWindowDbClick);
+    window.addEventListener('pointerdown', this.handlePointerDown);
 
     this.mountHouseFromIndexDb();
   }
 
-  private handleWindowDbClick = (e: MouseEvent) => {
-    const pointer = this.sceneConnector.getPointerPosition?.(e);
-    if (!pointer) return;
+  private handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape' && this.draftHouse) {
+      this.draftHouse.removeHouseLabel();
+      this.draftHouse.removeHouseArm();
+      this.sceneConnector.removeFromScene?.(this.draftHouse.mesh);
+      this.sceneConnector.enableOrbitControl();
+      this.draftHouse = null;
+      if (this.helperArmPlane) {
+        this.sceneConnector.removeFromScene?.(this.helperArmPlane);
+      }
+      window.removeEventListener('pointermove', this.handlePointerMove);
+      window.addEventListener('pointerup', this.handlePointerUp);
+    }
+    window.removeEventListener('keydown', this.handleKeyDown);
+  };
 
-    const intersect = this.sceneConnector.getIntersectWithGround?.(pointer);
-    if (!intersect) return;
+  private handleClickArmOfDraftHouse = () => {
+    const houseArm = this.draftHouse?.houseArm;
 
-    this.draftHouse?.moveHouseTo(intersect.point);
+    if (!houseArm || !this.draftHouse) return;
+
+    const geometry = new PlaneGeometry(100, 100);
+    const material = new MeshMatcapMaterial({ opacity: 0, transparent: true });
+    this.helperArmPlane = new Mesh(geometry, material);
+    this.helperArmPlane.position.y = houseArm.position.y;
+    this.helperArmPlane.rotateX(-Math.PI / 2);
+    this.helperArmPlane.renderOrder = 1;
+
+    this.sceneConnector.disableOrbitControl();
+
+    this.sceneConnector.addToScene?.(this.helperArmPlane);
+
+    const houseArmMaterial = houseArm.material as MeshLambertMaterial;
+
+    houseArmMaterial.color.set(0xffe921);
+
+    window.addEventListener('pointerup', this.handlePointerUp);
+    window.addEventListener('pointermove', this.handlePointerMove);
+  };
+
+  private handlePointerMove = (event: MouseEvent) => {
+    const pointer = this.sceneConnector.getPointerPosition?.(event);
+    if (pointer && this.draftHouse) {
+      this.moveHouseAlongGround(pointer, this.draftHouse);
+    }
+  };
+
+  private handlePointerUp = () => {
+    const houseArm = this.draftHouse?.houseArm;
+
+    if (!houseArm) return;
+
+    const houseArmMaterial = houseArm.material as MeshLambertMaterial;
+
+    houseArmMaterial.color.set(0x6794ab);
+
+    this.sceneConnector.enableOrbitControl();
+
+    if (this.helperArmPlane) {
+      this.sceneConnector.removeFromScene?.(this.helperArmPlane);
+    }
+
+    window.removeEventListener('pointermove', this.handlePointerMove);
+  };
+
+  private handlePointerDown = (event: MouseEvent) => {
+    const pointer = this.sceneConnector.getPointerPosition?.(event);
+
+    if (!this.draftHouse || !pointer) return;
+
+    const firstIntersect = this.sceneConnector.getIntersectWithSprite?.(
+      pointer,
+      this.draftHouse.mesh
+    );
+
+    const isClickedOnHouseArm = firstIntersect?.object === this.draftHouse.houseArm;
+
+    if (!isClickedOnHouseArm) return;
+
+    this.handleClickArmOfDraftHouse();
   };
 
   private handleSaveHouse = () => {
@@ -36,20 +110,29 @@ export class HousePainter {
 
   mountDraftHouseOnScene(assetTitle: string) {
     const house = this.createHouseByAssetTitle(assetTitle);
+
     if (!house) return;
 
     house.onSaveHouse = this.handleSaveHouse;
+
     house.setOpacity(0.5);
 
     this.draftHouse = house;
+
     this.sceneConnector.addToScene?.(house.mesh);
 
     house.createHouseLabel();
+
+    house.createHouseArm();
+
+    window.addEventListener('keydown', this.handleKeyDown);
   }
 
   saveHouse(house: House) {
     house.setOpacity(1);
     house.isMount = true;
+
+    this.housesMap.set(house.id, house);
 
     this.indexDB.saveHouseInfo({
       id: house.id,
@@ -60,6 +143,19 @@ export class HousePainter {
     });
   }
 
+  private moveHouseAlongGround(pointer: Vector2, house: House) {
+    const houseArm = house.houseArm;
+
+    if (!houseArm || !this.helperArmPlane) return;
+
+    const intersect = this.sceneConnector.getIntersectWithSprite?.(pointer, this.helperArmPlane);
+
+    if (!intersect) return;
+
+    house.mesh.position.x = intersect.point.x;
+    house.mesh.position.z = intersect.point.z;
+  }
+
   private createHouseByAssetTitle(assetTitle: string, id?: string) {
     const houseGLTF = this.assetMap.get(assetTitle);
     const assetConfig = assetsConfig.find(({ title }) => title === assetTitle);
@@ -67,6 +163,7 @@ export class HousePainter {
     if (!houseGLTF || !assetConfig) return;
 
     const houseMesh = houseGLTF.scene.clone(true);
+
     return new House(houseMesh, assetConfig, id);
   }
 
@@ -77,14 +174,17 @@ export class HousePainter {
       const house = this.createHouseByAssetTitle(info.assetTitle, info.id)!;
 
       house.name = info.houseName;
-      
+
       this.sceneConnector.addToScene?.(house.mesh);
 
       house.mesh.position.x = info.positionX;
       house.mesh.position.z = info.positionZ;
+
       house.isMount = true;
 
       house.createHouseLabel();
+
+      this.housesMap.set(house.id, house);
     }
   }
 }
